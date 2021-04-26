f3bd07f5307a107345ad9b1203065596a43f6c90
Cosmin Mihai Pascariu <cosmin.pascariu2@student.usv.ro>
2021-04-26 13:57:13 +0300

    Update

---
+++ InchirieriAuto/Persoana.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchirieriAuto
+{
+    class Persoana
+    {
+        public string nume;
+        public string prenume;
+        public string CNP;
+        public Persoana()
+        {
+            nume = "Neinregistrat";
+            prenume = "Neinregistrat";
+            CNP = "-------------";
+        }
+
+        public string PreluareDate()
+        {
+            return $"Nume: {nume}\nPrenume: {prenume}\nCNP: {CNP}";    
+        }
+    }
+}
---
+++ InchirieriAuto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchirieriAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoana p = new Persoana();
+            Console.WriteLine(p.PreluareDate());
+            Console.ReadKey();
+        }
+    }
+}
+
